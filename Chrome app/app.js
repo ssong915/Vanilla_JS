b32ae2f1d3f5e31397040dd542e8cb713397d387
ssong <yysk_915@naver.com>
2021-09-03 00:58:08 +0900

    2.0-3.8

--- Chrome app/app.js
+++ Chrome app/app.js
@@ -1,8 +1,9 @@
+const LoginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {    
+function onLoginSubmit() {    
     const username = loginInput.value;
     console.log(username);
 }
-loginButton.addEventListener("click",onLoginBtnClick)
+
+LoginForm.addEventListener("submit",onLoginSubmit);
