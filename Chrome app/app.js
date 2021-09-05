9d982e9219fb64a3f639012a9c175dd906b48db7
ssong <yysk_915@naver.com>
2021-09-05 11:57:02 +0900

    4.5 Saving Username

--- Chrome app/app.js
+++ Chrome app/app.js
@@ -5,10 +5,12 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
-  event.preventDefault();
+  event.preventDefault(); //브라우저의 기본동작 막기
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  greeting.innerText = `Hello ${username}`;
+  localStorage.setItem("username",username); //localStorage에 저장
+  greeting.innerText = `Hello ${username}`; //== "Hello " + username
+                        // string 안에 변수를 추가하고 싶다면 ${변수}로 추가
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
